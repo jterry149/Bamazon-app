b87b685c86560fcb1a3e3df42a2abfe224627645
JTerry <jessica.terry79@yahoo.com>
2018-11-10 00:30:20 -0600

    updated the bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -14,8 +14,16 @@ CREATE TABLE products (
 
 -- Insert data into the 'products' table --
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES  ('Orbea Occam Mountain Bike', 'Sporting & Outdoors',        1200.00, 30),
-		('Go Pro Hero 7', 'Electronics', 425.00, 627),	
-		('Coleman Campstove', 'Sporting & Outdoors', 89.95, 432),
-        ('Instinct Chicken Dog Food', 'Pet Supplies',56.95, 600);
+VALUES  ('Orbea Occam Mountain Bike', 'Sporting & Outdoors',1200.00, 30),
+		('Go Pro Hero 7', 'Electronics',425.00, 627),	
+		('Coleman Campstove', 'Sporting & Outdoors',89.95, 432),
+        ('Instinct Chicken Dog Food', 'Pet Supplies',56.95, 600),
+        ('Honeydew Melon', 'Grocery',3.25, 80),
+        ('Kraft Macaroni & Cheese', 'Grocery',2.25, 115),
+        ('Helio Outdoor Shower', 'Sporting & Outdoors',89.95, 57),
+        ('Sony Playstation Console', 'Electronics',299.95, 150),
+        ('Gala Apples', 'Grocery',3.95, 495),
+        ('Dove Bar soap', 'Hygiene',1.99, 450);
 
+-- Set the password to the localhost --
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
